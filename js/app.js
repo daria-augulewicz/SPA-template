4e9b363b8d4616389befa9f5f008365b704b3a57
Daria Augulewicz <daria.augulewicz@gmail.com>
2019-05-14 23:33:38 +0200

    Add JavaSctipt

--- js/app.js
+++ js/app.js
@@ -1,6 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
   
-  const a = 'xD';
-  console.log("działa");
+  // nested menu
+  const nestedMenu = document.querySelector (".nested-menu");
+  const menuTriggerEl = document.querySelector(".menu-trigger-el");
+
+  menuTriggerEl.addEventListener("mouseover", function () {
+    nestedMenu.style.display = "block";
+  });
+
+  nestedMenu.addEventListener("mouseout", function() {
+    this.style.display = "none";
+  });
+
+  //  read more-less
+  const readMoreLessBtn = document.querySelector(".read-more-less-btn");
+
+  function showHideText() {
+    const siblingText = this.previousElementSibling;
+    
+    if (siblingText.style.display === "none" || siblingText.style.display === "") {
+        siblingText.style.display = "block";
+        this.textContent = "Czytaj mniej";
+    } else {
+        siblingText.style.display = "none";
+        this.textContent = "Czytaj więcej";
+    }
+  }
   
+  readMoreLessBtn.addEventListener("click", showHideText);
 });
