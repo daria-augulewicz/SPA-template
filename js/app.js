8f89ee2fa2dbfe77ead6b19c7d5a9dee4d6faa79
Daria Augulewicz <daria.augulewicz@gmail.com>
2019-05-18 23:52:38 +0200

    Add JavaScript, CSS and HTML

--- js/app.js
+++ js/app.js
@@ -13,12 +13,12 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   //  read more-less
-  const readMoreLessBtn = document.querySelector(".read-more-less-btn");
+  const readMoreLessBtns = document.querySelectorAll(".read-more-less-btn");
 
   function showHideText() {
     const siblingText = this.previousElementSibling;
     
-    if (siblingText.style.display === "none" || siblingText.style.display === "") {
+    if (siblingText.style.display === "none" ||  siblingText.style.display === "") {
         siblingText.style.display = "block";
         this.textContent = "Czytaj mniej";
     } else {
@@ -27,5 +27,45 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
   
-  readMoreLessBtn.addEventListener("click", showHideText);
+  for (let i = 0; i < readMoreLessBtns.length; i++) {
+    readMoreLessBtns[i].addEventListener("click", showHideText);
+  }
+  
+//  newsletter
+  const input = document.querySelector(".newsletter-form input");
+  const newsletterBtn = document.querySelector(".newsletter-form button");
+  const formInfo = document.querySelector(".form-info");
+  
+  newsletterBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    formInfo.style.display = "block";
+    
+    if (input.value !== "") {
+      formInfo.textContent = "Zapisano do newslettera";
+      input.value = "";
+    } else {
+      formInfo.textContent = "Pole nie może być puste, podaj adres email";
+    }
+  });
+  
+  //slider
+  const slider = document.querySelector(".slider");
+  const sliderStage = document.querySelector(".slider-stage");
+  const slides = document.querySelectorAll(".slider li");
+  const prev = document.querySelector(".previous-arrow");
+  const next = document.querySelector(".next-arrow");
+  
+  const slideWidth = slides[0].clientWidth;
+  let currentIndex = 0;
+  let slidesNumber = slides.length - 1;
+  
+  function goToSlide(index) {
+    slider.style.left = index * (-slideWidth) + "px";
+    currentIndex = index;
+  }
+  
+  goToSlide();
+  
+  
+  
 });
